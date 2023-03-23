5aec5a6262243427cf6c1386b641f755b4a2e5ea
siammawii <siammawiikito@gmail.com>
2023-03-23 02:21:04 -0700

    girl

---
+++ 00-vector.R
@@ -0,0 +1,17 @@
+rm(list=ls())
+# variable types
+# character, numeric and logical
+a = 1 #a is a numeric variable with 1 value
+b = 2
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+x = c(1, 3, 10) # use c() function for multiple inputs
+y = c(8, 1, 100)
+
+# data types
+# vector
+# string split function
